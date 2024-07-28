10801fb8f392df7593bd03fe3531997975a22450
Cian <cian@mindhacker.com>
2024-07-27 19:24:35 -0700

    remove add function haha

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, flash, render_template, redirect, request
-from tasks import add
 import bot
 
 app = Flask(__name__)
@@ -12,15 +11,6 @@ app.register_blueprint(bot.bot_bp)
 @app.route("/")
 def main():
     return "Hello, World!"
-
-
-@app.route("/add", methods=["POST"])
-def add_inputs():
-    x = int(request.form["x"] or 0)
-    y = int(request.form["y"] or 0)
-    add.delay(x, y)
-    flash("Your addition job has been submitted.")
-    return redirect("/")
 
 
 if __name__ == "__main__":
